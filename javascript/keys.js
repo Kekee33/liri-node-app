be0b67d18ed09b40163a74c5b3b5acfd500b40fa
keisha <keisha@sample.com>
2017-07-24 11:23:53 -0400

    added intial files

---
+++ javascript/keys.js
@@ -0,0 +1,10 @@
+'''JavaScript
+console.log('this is loaded');
+
+exports.twitterKeys = {
+	consumer_key: '<V8a3a3Md6R5zFS68aGdxqTwYG>',
+	consumer_secret: '<44v7QEe3AF6wQTabvSHSEZLCvuqbFcuRVd9PFEhgvk3HsKUC29>',
+	access_token_key: '<889494619741933577-aM5NSIIs1j7h5E4tDTfQruXl7RLmNy7>',
+	access_token_secret: '<yNydHmCRbutInRZfGWpn9B8uMNkhHzs2gDCwCWciFt3Y8>',
+}
+'''
